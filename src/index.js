4ac96c96bd5406a02d1473830f8878ef9ca13b7f
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-17 09:55:25 -0700

    Added Material Design Icons.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import 'material-icons/css/material-icons.min.css';
+// Fonts should use CDN. Leaving this here for offline development.
+import 'material-design-icons/iconfont/MaterialIcons-Regular.svg';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,6 +24,7 @@ export default {
             loaders: ['babel']
          },
          { test: /\.css$/, loader: "style-loader!css-loader" },
+         { test: /\.png$/, loader: 'url-loader?limit=100000' },
          { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
          { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" }
       ]
