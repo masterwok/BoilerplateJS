e54d6496979eecac92d28f8dcd7d01698cd37a79
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-17 10:19:48 -0700

    Materialize fully imported.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@
 import 'material-design-icons/iconfont/MaterialIcons-Regular.svg';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+import jQuery from 'jquery';
 
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
