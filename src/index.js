a63a7260fd9444802f13a35aa1512c12d35ed01e
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-17 10:28:54 -0700

    Whoops, icons weren't actually working locally.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Fonts should use CDN. Leaving this here for offline development.
 import 'material-design-icons/iconfont/MaterialIcons-Regular.svg';
+import 'material-design-icons/iconfont/material-icons.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import jQuery from 'jquery';
