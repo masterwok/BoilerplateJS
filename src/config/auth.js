adb25174f342aa5d18c8c604b5a6e11c8b747727
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-18 15:58:26 -0700

    Removed unnecessary packages.

---
+++ src/config/auth.js
@@ -0,0 +1,7 @@
+export const config = {
+   facebook: {
+      clientID: 'your-secret-clientID-here', // your App ID
+      clientSecret: 'your-client-secret-here', // your App Secret
+      callbackURL: 'http://localhost:8080/auth/facebook/callback'
+   }
+};
