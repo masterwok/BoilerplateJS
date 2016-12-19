ed0be08ba60f4480364056b252ccbd1b19b95003
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-18 17:51:10 -0700

    Tweak.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import 'material-design-icons/iconfont/MaterialIcons-Regular.svg';
 import 'material-design-icons/iconfont/material-icons.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
-import jQuery from 'jquery';
+import $ from 'jquery';
 
 import './style/css/site.css';
 
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function () {
+   $('.button-collapse').sideNav();
+});
