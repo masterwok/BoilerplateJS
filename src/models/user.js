b73dfcc5171abec79bced13ce9a6a25531c4ad38
Jonathan Trowbridge <Jonathan.Trowbridge@gmail.com>
2016-12-23 00:08:53 -0700

    Cleaned up user module.

--- src/models/user.js
+++ src/models/user.js
@@ -1,47 +1,57 @@
 import mongoose from 'mongoose';
 import chalk from 'chalk';
 
-export default {
-   Schema: mongoose.model('User', mongoose.Schema({
-      facebook: {
-         id: String,
-         token: String,
-         email: String,
-         name: String
-      }
-   })),
-   getByFacebookId: function (id, done) {
-
-      this.Schema.findOne({
-         'facebook.id': id
-      }, (err, user) => {
-         if (err) {
-            console.log(chalk.red(`Error while retrieving user with facebook id: ${id}, ${err}`)); // eslint-disable-line no-console
-            return done(err);
-         }
-
-         done(null, user);
-      });
-   },
-   createFacebookUser: function (token, refreshToken, profile, done) {
-      let newUser = new this.Schema();
-
-      newUser.facebook.id = profile.id;
-      newUser.facebook.token = token;
-      newUser.facebook.name = `${profile.name.givenName} ${profile.name.familyName}`;
-
-      if(profile.emails.length > 0) {
-         // Grab the first email
-         newUser.facebook.email = profile.emails[0].value;
-      }
-
-      newUser.save((err) => {
-         if (err) {
-            console.log(chalk.red(`Error while creating a new facebook user: ${err}`)); // eslint-disable-line no-console
-            return done(err, null);
-         }
-
-         return done(null, newUser);
-      });
+// Schema definition for users
+var userSchema = mongoose.Schema({
+   facebook: {
+      id: String,
+      token: String,
+      email: String,
+      name: String
    }
+});
+
+userSchema.statics.getById = function (id, done) {
+   this.findById(id, (err, user) => {
+      done(err, user);
+   });
 };
+
+// Get a user by their facebook Id.
+userSchema.statics.getByFacebookId = function (id, done) {
+   this.findOne({
+      'facebook.id': id
+   }, (err, user) => {
+      if (err) {
+         console.log(chalk.red(`Error while retrieving user with facebook id: ${id}, ${err}`)); // eslint-disable-line no-console
+         return done(err);
+      }
+
+      done(null, user);
+   });
+};
+
+// Create a facebook user
+userSchema.statics.createFacebookUser = function (token, refreshToken, profile, done) {
+   let newUser = new this.Schema();
+
+   newUser.facebook.id = profile.id;
+   newUser.facebook.token = token;
+   newUser.facebook.name = `${profile.name.givenName} ${profile.name.familyName}`;
+
+   if (profile.emails.length > 0) {
+      // Grab the first email
+      newUser.facebook.email = profile.emails[0].value;
+   }
+
+   newUser.save((err) => {
+      if (err) {
+         console.log(chalk.red(`Error while creating a new facebook user: ${err}`)); // eslint-disable-line no-console
+         return done(err, null);
+      }
+
+      return done(null, newUser);
+   });
+}
+
+export default mongoose.model('User', userSchema);
