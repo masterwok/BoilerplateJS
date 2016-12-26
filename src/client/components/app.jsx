d66c5fb218ed4215fb3e419e84c29e2350882244
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-26 00:40:07 -0700

    Added React to project. Moved some scripts to clientside.

---
+++ src/client/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return <h1>App hit</h1>
+   }
+
+};
