3c476d948279c76fa4862fd0ffbe479a5baa5926
Jonathan Trowbridge <Jonathan Trowbridge>
2016-12-27 23:36:07 -0700

    AppBar lifts state to App which in turn updates the drawer.

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import NavigationBar from './navigation/NavigationBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Drawer from 'material-ui/Drawer';
 
 export default class App extends React.Component {
    constructor(props) {
@@ -11,13 +12,26 @@ export default class App extends React.Component {
       injectTapEventPlugin();
 
       this.state = {
-         user: user
+         user: user,
+         drawerOpen: true
       };
+
+      this.toggleDrawer = this.toggleDrawer.bind(this);
+   }
+
+   toggleDrawer() {
+      this.setState({ drawerOpen: !this.state.drawerOpen });
    }
 
    render() {
       return <MuiThemeProvider>
-         <NavigationBar />
+         <div>
+            <NavigationBar drawerOpen={this.state.drawerOpen} toggleDrawer={this.toggleDrawer} />
+            <Drawer
+               docked={true}
+               open={this.state.drawerOpen}>
+            </Drawer>
+         </div>
       </MuiThemeProvider>
    }
 
--- src/client/components/navigation/NavigationBar.jsx
+++ src/client/components/navigation/NavigationBar.jsx
@@ -2,35 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 
 export default class NavigationBar extends React.Component {
    constructor(props) {
       super(props);
-
-      this.state = { open: true };
-
-      this.handleToggle = this.handleToggle.bind(this)
-   }
-
-   handleToggle() {
-      console.log(this.state);
-      this.setState({open: !this.state.open});
    }
 
    render() {
       return <div>
          <AppBar
-            className={this.state.open ? 'app-bar expanded' : 'app-bar'}
-            onLeftIconButtonTouchTap={this.handleToggle}
-            title="Development"
-            />
-            <Drawer
-            docked={true}
-            open={this.state.open}>
-
-            </Drawer>
+            className={this.props.drawerOpen ? 'app-bar expanded' : 'app-bar'}
+            onLeftIconButtonTouchTap={this.props.toggleDrawer}
+            title="Development" />
       </div>
    }
 
