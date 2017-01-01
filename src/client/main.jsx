4c8250b872e8a69d9e40c42281523355c294cfd3
Jonathan Trowbridge <Jonathan.Trowbridge@gmail.com>
2017-01-01 13:24:23 -0700

    Sign in/out (need to fix button alignment).

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -5,13 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// This block of code will eventually move into the navigation
-// or sidebar React component
-// import $ from 'jquery';
-// $(document).ready(function () {
-//    $('.button-collapse').sideNav();
-// });
-
 ReactDOM.render(
    <App user={window.user}/>,
    document.getElementById('root')
