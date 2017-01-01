62d6694ac72fe4645bbecb83d48838a64949cf7f
Jonathan Trowbridge <Jonathan.Trowbridge@gmail.com>
2017-01-01 16:04:08 -0700

    Added react-router node module and added some placeholder on the home view.

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -3,6 +3,9 @@ import NavigationBar from './navigation/NavigationBar';
 import NavigationDrawer from './navigation/NavigationDrawer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import TextField from 'material-ui/TextField';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 export default class App extends React.Component {
    constructor(props) {
@@ -35,6 +38,23 @@ export default class App extends React.Component {
                user={this.state.user}>
             </NavigationBar>
             <NavigationDrawer drawerOpen={this.state.drawerOpen}/>
+            <div className={`app-content ${this.state.drawerOpen ? 'expanded' : ''}`} >
+               <div className="row center-xs">
+                  <div className="col-xs-6">
+                     <div className="box">
+                        <TextField
+                           hintText="Search"
+                           floatingLabelText="Find Recipes"
+                           fullWidth={true}
+                        />
+                     </div>
+                  </div>
+               </div>
+               <FloatingActionButton secondary={true} style={{ position: "fixed", bottom: "20", right: "20" }}>
+                  <ContentAdd />
+               </FloatingActionButton>
+
+            </div>
          </div>
       </MuiThemeProvider>
    }
