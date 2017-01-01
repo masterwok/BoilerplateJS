7284429421c97302875ae17821b2405832ff3ca7
Jonathan Trowbridge <Jonathan Trowbridge>
2017-01-01 13:43:57 -0700

    Added font-awesome, cleaned up sign in button.

--- src/client/components/navigation/NavigationBar.jsx
+++ src/client/components/navigation/NavigationBar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
-import NavigationBarIconMenu from 'client/components/navigation/NavigationBarIconMenu';
+import FlatButton from 'material-ui/FlatButton';
+import NavigationBarUserMenu from 'client/components/navigation/NavigationBarUserMenu';
+import FontIcon from 'material-ui/FontIcon';
 
 export default class NavigationBar extends React.Component {
    constructor(props) {
       super(props);
+
+      this.signIn = this.signIn.bind(this);
    }
+
+   signIn() {
+      window.location = '/auth/facebook';
+   }
+
 
    render() {
       return <AppBar
@@ -13,7 +22,11 @@ export default class NavigationBar extends React.Component {
             ? 'app-bar expanded'
             : 'app-bar'}
             onLeftIconButtonTouchTap={this.props.toggleDrawer}
-            iconElementRight={<NavigationBarIconMenu user={this.props.user}/>}
+            iconElementRight={
+               this.props.user
+               ? <NavigationBarUserMenu user={this.props.user}/>
+               : <FlatButton label="Sign In" icon={<FontIcon className="fa fa-facebook-official"/>} onTouchTap={this.signIn}/>
+            }
             title="Development"/>
    }
 
