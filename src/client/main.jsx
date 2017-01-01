2cb40917603de392cbe94a2608ee4f7802f2e303
Jonathan Trowbridge <Jonathan Trowbridge>
2017-01-01 16:04:08 -0700

    Added react-router node module and added some placeholder on the home view.

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,6 +2,7 @@
 import 'material-design-icons/iconfont/MaterialIcons-Regular.svg';
 import 'material-design-icons/iconfont/material-icons.css';
 import 'font-awesome/css/font-awesome.css'
+import 'flexboxgrid/css/flexboxgrid.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
