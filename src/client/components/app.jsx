4a39f4b9060484b87a1c7ef8fcf3eabaddbb0eea
Jonathan Trowbridge <Jonathan Trowbridge>
2017-01-02 19:07:40 -0700

    Added first route!

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,62 +1,29 @@
-import React from 'react';
-import NavigationBar from './navigation/NavigationBar';
-import NavigationDrawer from './navigation/NavigationDrawer';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, {Component} from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import TextField from 'material-ui/TextField';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
+import AppContainer from 'client/components/AppContainer';
+import SearchRecipes from 'client/components/recipes/Search';
+import {
+   Router,
+   Route,
+   Link,
+   IndexRoute,
+   hashHistory,
+   browserHistory
+} from 'react-router'
 
-export default class App extends React.Component {
+export default class App extends Component {
    constructor(props) {
       super(props);
 
       injectTapEventPlugin();
-
-      this.state = {
-         user: user,
-         drawerOpen: false
-      };
-
-      this.toggleDrawer = this
-         .toggleDrawer
-         .bind(this);
-   }
-
-   toggleDrawer() {
-      this.setState({
-         drawerOpen: !this.state.drawerOpen
-      });
    }
 
    render() {
-      return <MuiThemeProvider>
-         <div>
-            <NavigationBar
-               drawerOpen={this.state.drawerOpen}
-               toggleDrawer={this.toggleDrawer}
-               user={this.state.user}>
-            </NavigationBar>
-            <NavigationDrawer drawerOpen={this.state.drawerOpen}/>
-            <div className={`app-content ${this.state.drawerOpen ? 'expanded' : ''}`} >
-               <div className="row center-xs">
-                  <div className="col-xs-6">
-                     <div className="box">
-                        <TextField
-                           hintText="Search"
-                           floatingLabelText="Find Recipes"
-                           fullWidth={true}
-                        />
-                     </div>
-                  </div>
-               </div>
-               <FloatingActionButton secondary={true} style={{ position: "fixed", bottom: "20", right: "20" }}>
-                  <ContentAdd />
-               </FloatingActionButton>
-
-            </div>
-         </div>
-      </MuiThemeProvider>
+      return <Router history={hashHistory}>
+         <Route path='/' component={AppContainer}>
+            <IndexRoute component={SearchRecipes}/>
+         </Route>
+      </Router>;
    }
 
 };
